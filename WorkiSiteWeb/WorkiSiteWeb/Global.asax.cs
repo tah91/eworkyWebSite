a5f2fc3f99ffe2824dcae4cf040b7716bfd31f59
Tahir <tah.iftikhar@gmail.com>
2011-10-01 15:44:24 +0200

    disable mobile version

--- WorkiSiteWeb/WorkiSiteWeb/Global.asax.cs
+++ WorkiSiteWeb/WorkiSiteWeb/Global.asax.cs
@@ -299,7 +299,7 @@ namespace Worki.Web
 			//routes
             AreaRegistration.RegisterAllAreas();
 			XmlSiteMapController.RegisterRoutes(RouteTable.Routes);
-            GlobalFilters.Filters.Add(new RedirectMobileDevicesToMobileAreaAttribute(), 1);
+            //GlobalFilters.Filters.Add(new RedirectMobileDevicesToMobileAreaAttribute(), 1);
             RegisterRoutes(RouteTable.Routes);
 
 			//model binder
