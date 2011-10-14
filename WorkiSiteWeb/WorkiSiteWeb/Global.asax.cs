a6551cff5747e80af92de8896027e5fa6f074b3b
Tahir <tah.iftikhar@gmail.com>
2011-10-14 19:28:43 +0200

    rental layout fix

--- WorkiSiteWeb/WorkiSiteWeb/Global.asax.cs
+++ WorkiSiteWeb/WorkiSiteWeb/Global.asax.cs
@@ -197,6 +197,13 @@ namespace Worki.Web
             );
 
             routes.MapRoute(
+                "", // Nom d'itinéraire
+                "annonces/{action}/{id}", // URL avec des paramètres
+                new { controller = "Rental", action = "Index", id = UrlParameter.Optional }, // Paramètres par défaut
+                new string[] { "Worki.Web.Controllers" }
+            );
+
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
 				new { controller = "Home", action = "Index", id = UrlParameter.Optional }, // Paramètres par défaut
