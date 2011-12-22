8fb1e7741edfb938e0adc7e02dce6acf6a252081
Mika <Eworky@.(none)>
2011-12-22 11:42:11 +0100

    fix bug on rating + ordered 4square and eworky comments

--- WorkiSiteWeb/WorkiSiteWeb/Controllers/LocalisationController.cs
+++ WorkiSiteWeb/WorkiSiteWeb/Controllers/LocalisationController.cs
@@ -87,7 +87,18 @@ namespace Worki.Web.Controllers
                         var tips = tipsJSON["response"]["tips"];
                         foreach (var tip in tips["items"])
                         {
-                            var com = new Comment { Date = new DateTime(1970, 1, 1).AddTicks((long)tip["createdAt"] * 10000000), Post = (string)tip["text"], Member = new Member { MemberMainData = new MemberMainData { FirstName = (string)tip["user"]["firstName"], LastName = (string)tip["user"]["lastName"], Avatar = (string)tip["user"]["photo"] } }, Rating = 3, RatingDispo = 3, RatingPrice = 3, RatingWelcome = 3, RatingWifi = 3 };
+                            var com = new Comment { Localisation = container.Localisation,
+                                                    Date = new DateTime(1970, 1, 1).AddTicks((long)tip["createdAt"] * 10000000),
+                                                    Post = (string)tip["text"],
+                                                    Member = new Member { MemberMainData = new MemberMainData { FirstName = (string)tip["user"]["firstName"],
+                                                                                                                LastName = (string)tip["user"]["lastName"],
+                                                                                                                Avatar = (string)tip["user"]["photo"] } },
+                                                    Rating = 3,
+                                                    RatingDispo = 3,
+                                                    RatingPrice = 3,
+                                                    RatingWelcome = 3,
+                                                    RatingWifi = 3 };
+
                             container.Localisation.Comments.Add(com);
                         }
                     }
