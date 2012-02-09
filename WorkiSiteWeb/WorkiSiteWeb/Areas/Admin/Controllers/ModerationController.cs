0b77925c7fc8f3d497561ef0f29294f3d1f9fa82
Tahir <tah.iftikhar@gmail.com>
2012-02-09 13:31:49 +0100

    import en desc

--- WorkiSiteWeb/WorkiSiteWeb/Areas/Admin/Controllers/ModerationController.cs
+++ WorkiSiteWeb/WorkiSiteWeb/Areas/Admin/Controllers/ModerationController.cs
@@ -180,135 +180,219 @@ namespace Worki.Web.Areas.Admin.Controllers
             return View(viewModel);
         }
 
-        /// <summary>
-        /// Prepares a web page to upload a csv file
-        /// </summary>
-        /// <param name="page">The page to display</param>
-        /// <returns>The action result.</returns>
-        [AcceptVerbs(HttpVerbs.Post)]
-        [ValidateAntiForgeryToken]
-        public virtual ActionResult IndexImport(FormCollection collection)
-        {
-            char CSV_SEPARATOR = ';';
-            string featureTrueIndicator = Worki.Resources.Views.Shared.SharedString.Yes; // by default, it's false. It's true only for the string
-            int nbCol = 20;
-            bool isHeaderLine = false;
-            if (collection.Get("importCsvHeader") != null && collection.Get("importCsvHeader") == "on")
-                isHeaderLine = true;
-            int nbLocalisationsAdded = 0;
-            string listLocalisationsAlreadyInDB = "";
-            var context = ModelFactory.GetUnitOfWork();
-            var mRepo = ModelFactory.GetRepository<IMemberRepository>(context);
-            var lRepo = ModelFactory.GetRepository<ILocalisationRepository>(context);
-            foreach (string name in Request.Files)
-            {
-                try
-                {
-                    var postedFile = Request.Files[name];
-                    if (postedFile == null || string.IsNullOrEmpty(postedFile.FileName))
-                        continue;
+		///// <summary>
+		///// Prepares a web page to upload a csv file
+		///// </summary>
+		///// <param name="page">The page to display</param>
+		///// <returns>The action result.</returns>
+		//[AcceptVerbs(HttpVerbs.Post)]
+		//[ValidateAntiForgeryToken]
+		//public virtual ActionResult IndexImport(FormCollection collection)
+		//{
+		//    char CSV_SEPARATOR = ';';
+		//    string featureTrueIndicator = Worki.Resources.Views.Shared.SharedString.Yes; // by default, it's false. It's true only for the string
+		//    int nbCol = 20;
+		//    bool isHeaderLine = false;
+		//    if (collection.Get("importCsvHeader") != null && collection.Get("importCsvHeader") == "on")
+		//        isHeaderLine = true;
+		//    int nbLocalisationsAdded = 0;
+		//    string listLocalisationsAlreadyInDB = "";
+		//    var context = ModelFactory.GetUnitOfWork();
+		//    var mRepo = ModelFactory.GetRepository<IMemberRepository>(context);
+		//    var lRepo = ModelFactory.GetRepository<ILocalisationRepository>(context);
+		//    foreach (string name in Request.Files)
+		//    {
+		//        try
+		//        {
+		//            var postedFile = Request.Files[name];
+		//            if (postedFile == null || string.IsNullOrEmpty(postedFile.FileName))
+		//                continue;
 
-                    int fileLen;
-                    fileLen = postedFile.ContentLength;
-                    byte[] input = new byte[fileLen];
+		//            int fileLen;
+		//            fileLen = postedFile.ContentLength;
+		//            byte[] input = new byte[fileLen];
 
-                    StreamReader sr = new StreamReader(postedFile.InputStream, System.Text.Encoding.Default); // Using of encoding to don't loose french accents
+		//            StreamReader sr = new StreamReader(postedFile.InputStream, System.Text.Encoding.Default); // Using of encoding to don't loose french accents
 
-                    // Because we can have a csv line in multiple line in files (because in a value, we can have line-return), we have to check it
-                    string fullCSVLine = "";
-                    while (sr.Peek() >= 0) // Read of each line of CSV file
-                    {
-                        fullCSVLine += sr.ReadLine();
-                        string[] infosLocalisation = fullCSVLine.Split(CSV_SEPARATOR);
+		//            // Because we can have a csv line in multiple line in files (because in a value, we can have line-return), we have to check it
+		//            string fullCSVLine = "";
+		//            while (sr.Peek() >= 0) // Read of each line of CSV file
+		//            {
+		//                fullCSVLine += sr.ReadLine();
+		//                string[] infosLocalisation = fullCSVLine.Split(CSV_SEPARATOR);
 
-                        // We have not the full CSV line
-                        if (infosLocalisation.Length < nbCol)
-                            continue;
+		//                // We have not the full CSV line
+		//                if (infosLocalisation.Length < nbCol)
+		//                    continue;
 
-                        if (!isHeaderLine)
-                        {
-                            var localisationToAdd = new Localisation();
-                            localisationToAdd.Name = infosLocalisation[1];
-                            localisationToAdd.Adress = infosLocalisation[2];
-                            localisationToAdd.PostalCode = infosLocalisation[3];
-                            localisationToAdd.City = infosLocalisation[4];
-                            localisationToAdd.Country = infosLocalisation[5];
-                            localisationToAdd.PhoneNumber = infosLocalisation[6];
-                            localisationToAdd.Fax = infosLocalisation[7];
-                            localisationToAdd.Mail = infosLocalisation[8];
-                            localisationToAdd.WebSite = infosLocalisation[9];
-                            localisationToAdd.Description = infosLocalisation[10];
-                            double latitude, longitude;
-                            Double.TryParse(infosLocalisation[18].Replace(',', '.'), out latitude);
-                            Double.TryParse(infosLocalisation[19].Replace(',', '.'), out longitude);
-                            localisationToAdd.Latitude = latitude;
-                            localisationToAdd.Longitude = longitude;
+		//                if (!isHeaderLine)
+		//                {
+		//                    var localisationToAdd = new Localisation();
+		//                    localisationToAdd.Name = infosLocalisation[1];
+		//                    localisationToAdd.Adress = infosLocalisation[2];
+		//                    localisationToAdd.PostalCode = infosLocalisation[3];
+		//                    localisationToAdd.City = infosLocalisation[4];
+		//                    localisationToAdd.Country = infosLocalisation[5];
+		//                    localisationToAdd.PhoneNumber = infosLocalisation[6];
+		//                    localisationToAdd.Fax = infosLocalisation[7];
+		//                    localisationToAdd.Mail = infosLocalisation[8];
+		//                    localisationToAdd.WebSite = infosLocalisation[9];
+		//                    localisationToAdd.Description = infosLocalisation[10];
+		//                    double latitude, longitude;
+		//                    Double.TryParse(infosLocalisation[18].Replace(',', '.'), out latitude);
+		//                    Double.TryParse(infosLocalisation[19].Replace(',', '.'), out longitude);
+		//                    localisationToAdd.Latitude = latitude;
+		//                    localisationToAdd.Longitude = longitude;
 
-                            // Description in English : localisationToAdd.DescriptionEnglish = infosLocalisation[11]; ??
-                            var member = mRepo.GetMember(infosLocalisation[0]);
-                            localisationToAdd.SetOwner(member.MemberId);
+		//                    // Description in English : localisationToAdd.DescriptionEnglish = infosLocalisation[11]; ??
+		//                    var member = mRepo.GetMember(infosLocalisation[0]);
+		//                    localisationToAdd.SetOwner(member.MemberId);
 
-                            if (infosLocalisation[12].Trim().ToLower() == featureTrueIndicator.ToLower())
-                                localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.VisioRoom, OfferID = (int)FeatureType.VisioRoom });
-                            if (infosLocalisation[13].Trim().ToLower() == featureTrueIndicator.ToLower())
-                                localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.MeetingRoom, OfferID = (int)FeatureType.MeetingRoom });
-                            if (infosLocalisation[14].Trim().ToLower() == featureTrueIndicator.ToLower())
-                                localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.SeminarRoom, OfferID = (int)FeatureType.SeminarRoom });
-                            if (infosLocalisation[15].Trim().ToLower() == featureTrueIndicator.ToLower())
-                                localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.BuisnessLounge, OfferID = (int)FeatureType.WorkingPlace });
-                            if (infosLocalisation[16].Trim().ToLower() == featureTrueIndicator.ToLower())
-                                localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.Workstation, OfferID = (int)FeatureType.WorkingPlace });
-                            if (infosLocalisation[17].Trim().ToLower() == featureTrueIndicator.ToLower())
-                                localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.Desktop, OfferID = (int)FeatureType.WorkingPlace });
+		//                    if (infosLocalisation[12].Trim().ToLower() == featureTrueIndicator.ToLower())
+		//                        localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.VisioRoom, OfferID = (int)FeatureType.VisioRoom });
+		//                    if (infosLocalisation[13].Trim().ToLower() == featureTrueIndicator.ToLower())
+		//                        localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.MeetingRoom, OfferID = (int)FeatureType.MeetingRoom });
+		//                    if (infosLocalisation[14].Trim().ToLower() == featureTrueIndicator.ToLower())
+		//                        localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.SeminarRoom, OfferID = (int)FeatureType.SeminarRoom });
+		//                    if (infosLocalisation[15].Trim().ToLower() == featureTrueIndicator.ToLower())
+		//                        localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.BuisnessLounge, OfferID = (int)FeatureType.WorkingPlace });
+		//                    if (infosLocalisation[16].Trim().ToLower() == featureTrueIndicator.ToLower())
+		//                        localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.Workstation, OfferID = (int)FeatureType.WorkingPlace });
+		//                    if (infosLocalisation[17].Trim().ToLower() == featureTrueIndicator.ToLower())
+		//                        localisationToAdd.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.Desktop, OfferID = (int)FeatureType.WorkingPlace });
 
-                            localisationToAdd.TypeValue = (int)LocalisationType.BuisnessCenter;
+		//                    localisationToAdd.TypeValue = (int)LocalisationType.BuisnessCenter;
 
-                            var similarLoc = (from loc
-                                                 in lRepo.FindSimilarLocalisation((float)localisationToAdd.Latitude, (float)localisationToAdd.Longitude)
-                                              where string.Compare(loc.Name, localisationToAdd.Name, StringComparison.InvariantCultureIgnoreCase) == 0
-                                              select loc).ToList();
-                            if (similarLoc.Count > 0)
-                            {
-                                //temp, add wifi
-                                if (similarLoc.Count() == 1)
-                                {
-                                    var loc = similarLoc[0];
-                                    if (!loc.HasFeature(Feature.Wifi_Free))
-                                    {
-                                        var l = lRepo.Get(loc.ID);
-                                        l.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.Wifi_Free, OfferID = (int)FeatureType.General });
-                                    }
-                                }
-                                listLocalisationsAlreadyInDB += "&bull; " + localisationToAdd.Name + "<br />";
-                            }
-                            else
-                            {
-                                lRepo.Add(localisationToAdd);
-                                member.MemberEditions.Add(new MemberEdition { ModificationDate = DateTime.UtcNow, LocalisationId = localisationToAdd.ID, ModificationType = (int)EditionType.Creation });
-                                nbLocalisationsAdded++;
-                            }
-                        }
-                        else
-                            isHeaderLine = false; // because we have skipped the first line
+		//                    var similarLoc = (from loc
+		//                                         in lRepo.FindSimilarLocalisation((float)localisationToAdd.Latitude, (float)localisationToAdd.Longitude)
+		//                                      where string.Compare(loc.Name, localisationToAdd.Name, StringComparison.InvariantCultureIgnoreCase) == 0
+		//                                      select loc).ToList();
+		//                    if (similarLoc.Count > 0)
+		//                    {
+		//                        //temp, add wifi
+		//                        if (similarLoc.Count() == 1)
+		//                        {
+		//                            var loc = similarLoc[0];
+		//                            if (!loc.HasFeature(Feature.Wifi_Free))
+		//                            {
+		//                                var l = lRepo.Get(loc.ID);
+		//                                l.LocalisationFeatures.Add(new LocalisationFeature { FeatureID = (int)Feature.Wifi_Free, OfferID = (int)FeatureType.General });
+		//                            }
+		//                        }
+		//                        listLocalisationsAlreadyInDB += "&bull; " + localisationToAdd.Name + "<br />";
+		//                    }
+		//                    else
+		//                    {
+		//                        lRepo.Add(localisationToAdd);
+		//                        member.MemberEditions.Add(new MemberEdition { ModificationDate = DateTime.UtcNow, LocalisationId = localisationToAdd.ID, ModificationType = (int)EditionType.Creation });
+		//                        nbLocalisationsAdded++;
+		//                    }
+		//                }
+		//                else
+		//                    isHeaderLine = false; // because we have skipped the first line
 
-                        fullCSVLine = ""; // Reinitialization because we have found the full CVS line
+		//                fullCSVLine = ""; // Reinitialization because we have found the full CVS line
 
-                    }
-                    context.Commit();
-                }
-                catch (Exception ex)
-                {
-                    _Logger.Error("Edit", ex);
-                    ModelState.AddModelError("", string.Format(Worki.Resources.Views.Shared.SharedString.AddLocError, nbLocalisationsAdded, ex.Message));
-                    context.Complete();
-                }
-            }
+		//            }
+		//            context.Commit();
+		//        }
+		//        catch (Exception ex)
+		//        {
+		//            _Logger.Error("Edit", ex);
+		//            ModelState.AddModelError("", string.Format(Worki.Resources.Views.Shared.SharedString.AddLocError, nbLocalisationsAdded, ex.Message));
+		//            context.Complete();
+		//        }
+		//    }
 
-            AdminImportViewModel viewModel = new AdminImportViewModel();
-            viewModel.resultMessage = string.Format(Worki.Resources.Views.Shared.SharedString.LocAdded, nbLocalisationsAdded);
-            viewModel.localisationsAlreadyInDB = listLocalisationsAlreadyInDB;
-            return View(viewModel);
-        }
+		//    AdminImportViewModel viewModel = new AdminImportViewModel();
+		//    viewModel.resultMessage = string.Format(Worki.Resources.Views.Shared.SharedString.LocAdded, nbLocalisationsAdded);
+		//    viewModel.localisationsAlreadyInDB = listLocalisationsAlreadyInDB;
+		//    return View(viewModel);
+		//}
+
+		/// <summary>
+		/// Prepares a web page to upload a csv file
+		/// </summary>
+		/// <param name="page">The page to display</param>
+		/// <returns>The action result.</returns>
+		[AcceptVerbs(HttpVerbs.Post)]
+		[ValidateAntiForgeryToken]
+		public virtual ActionResult IndexImport(FormCollection collection)
+		{
+			char CSV_SEPARATOR = ';';
+			string featureTrueIndicator = Worki.Resources.Views.Shared.SharedString.Yes; // by default, it's false. It's true only for the string
+			int nbCol = 5;
+			bool isHeaderLine = false;
+			if (collection.Get("importCsvHeader") != null && collection.Get("importCsvHeader") == "on")
+				isHeaderLine = true;
+			int nbLocalisationsAdded = 0;
+			string listLocalisationsAlreadyInDB = "";
+			var context = ModelFactory.GetUnitOfWork();
+			var mRepo = ModelFactory.GetRepository<IMemberRepository>(context);
+			var lRepo = ModelFactory.GetRepository<ILocalisationRepository>(context);
+			foreach (string name in Request.Files)
+			{
+				try
+				{
+					var postedFile = Request.Files[name];
+					if (postedFile == null || string.IsNullOrEmpty(postedFile.FileName))
+						continue;
+
+					int fileLen;
+					fileLen = postedFile.ContentLength;
+					byte[] input = new byte[fileLen];
+
+					StreamReader sr = new StreamReader(postedFile.InputStream, System.Text.Encoding.Default); // Using of encoding to don't loose french accents
+
+					// Because we can have a csv line in multiple line in files (because in a value, we can have line-return), we have to check it
+					string fullCSVLine = "";
+					while (sr.Peek() >= 0) // Read of each line of CSV file
+					{
+						fullCSVLine += sr.ReadLine();
+						string[] infosLocalisation = fullCSVLine.Split(CSV_SEPARATOR);
+
+						// We have not the full CSV line
+						if (infosLocalisation.Length < nbCol)
+							continue;
+
+						if (!isHeaderLine)
+						{
+							var locName = infosLocalisation[1];
+							var enDesc = infosLocalisation[3];
+
+							var loc = lRepo.Get(l => string.Compare(l.Name, locName, StringComparison.InvariantCultureIgnoreCase) == 0);
+
+							if (loc ==null)
+							{
+								listLocalisationsAlreadyInDB += "&bull; " + locName + "<br />";
+							}
+							else
+							{
+								loc.DescriptionEn = enDesc;
+								nbLocalisationsAdded++;
+							}
+						}
+						else
+							isHeaderLine = false; // because we have skipped the first line
+
+						fullCSVLine = ""; // Reinitialization because we have found the full CVS line
+
+					}
+					context.Commit();
+				}
+				catch (Exception ex)
+				{
+					_Logger.Error("Edit", ex);
+					ModelState.AddModelError("", string.Format(Worki.Resources.Views.Shared.SharedString.AddLocError, nbLocalisationsAdded, ex.Message));
+					context.Complete();
+				}
+			}
+
+			AdminImportViewModel viewModel = new AdminImportViewModel();
+			viewModel.resultMessage = string.Format(Worki.Resources.Views.Shared.SharedString.LocAdded, nbLocalisationsAdded);
+			viewModel.localisationsAlreadyInDB = listLocalisationsAlreadyInDB;
+			return View(viewModel);
+		}
 
         #endregion
 
